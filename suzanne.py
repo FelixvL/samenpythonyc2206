b65f84ea637002e3d6ac326fd0ee5a4c924663c4
FelixvL <digitaleservice@gmail.com>
2022-06-24 10:28:31 +0200

    database

--- suzanne.py
+++ suzanne.py
@@ -1,2 +1,19 @@
+import mysql.connector
+from flask import jsonify
+
 def methodevansuzanne():
-	return "methodevansuzanne"
+	print("naar de database")
+	mydb = mysql.connector.connect(
+  		host="localhost", 
+  		user="root",
+  		password="",
+  		database="patat"
+	)
+
+	mycursor = mydb.cursor()
+	mycursor.execute("SELECT * FROM quote")
+	recordset = mycursor.fetchall()
+	print(recordset)
+
+	return jsonify(list(recordset))
+
