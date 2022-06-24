3a3c9427efcf10c1f3ba569575e0f373cf8f64f4
felixvl <digitaleservice@gmail.com>
2022-06-24 16:25:16 +0200

    toevoegen quote gemaakt

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
+
 import felix
 import nelleke
 import suzanne
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route("/")
+@cross_origin()
 def hello_world():
     return "<p>Hello, World!</p>"
 
 
 @app.route("/felixeerste")
+@cross_origin()
 def felixeerste():
     return felix.methodevanfelix()
 
 @app.route("/nellekeeerste")
+@cross_origin()
 def nellekeeerste():
     return nelleke.methodevannelleke()
 
 @app.route("/suzanneeerste")
+@cross_origin()
 def suzanneeerste():
     return suzanne.methodevansuzanne()
+
+@app.route("/aanmakenquote/<name>")
+def quoteaanmaken(name):
+    felix.aanmakenquote(name)
+    return "hij doet het"
--- felix.py
+++ felix.py
@@ -15,3 +15,17 @@ def methodevanfelix():
 	print(recordset)
 	return jsonify(list(recordset))
 
+def aanmakenquote(denaam):
+	mydb = mysql.connector.connect(
+		host="localhost",  #port erbij indien mac
+		user="root",
+		password="",
+		database="patat"
+	)
+	mycursor = mydb.cursor()
+	sql = "INSERT INTO quote (tekst, auteur) VALUES (%s, %s)"
+	val = (denaam, "harry")
+	mycursor.execute(sql, val)
+	mydb.commit()
+	print(mycursor.rowcount, "record inserted.")
+
