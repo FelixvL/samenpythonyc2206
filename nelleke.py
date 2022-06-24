7ac1f33e94b521a9111ae0c70bb64f6220849474
99nel <nvdoorn@gmail.com>
2022-06-24 10:32:19 +0200

    abc

--- nelleke.py
+++ nelleke.py
@@ -1,2 +1,18 @@
+import mysql.connector
+from flask import jsonify
+
+
 def methodevannelleke():
-	return "methodevannelleke"
+	print("naar de database")
+	mydb = mysql.connector.connect(
+		host="localhost",  #port erbij indien mac
+		user="root",
+		password="",
+		database="patat"
+)
+	mycursor = mydb.cursor()
+	mycursor.execute("SELECT * FROM quote")
+	recordset = mycursor.fetchall()
+	print(recordset)
+	return jsonify(list(recordset))
+
