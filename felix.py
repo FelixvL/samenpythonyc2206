d8420a52f1d2563b640d595bb9c5a228d4675fe2
felixvl <digitaleservice@gmail.com>
2022-06-24 10:25:25 +0200

    database call werkt

--- felix.py
+++ felix.py
@@ -1,2 +1,17 @@
+import mysql.connector
+from flask import jsonify
+
 def methodevanfelix():
-    return "methodevanfelix"
+	print("naar de database")
+	mydb = mysql.connector.connect(
+		host="localhost",  #port erbij indien mac
+		user="root",
+		password="",
+		database="patat"
+	)
+	mycursor = mydb.cursor()
+	mycursor.execute("SELECT * FROM quote")
+	recordset = mycursor.fetchall()
+	print(recordset)
+	return jsonify(list(recordset))
+
