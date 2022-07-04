da0a95ed1c8b083537474f851833dcdd8e536d41
FelixvL <digitaleservice@gmail.com>
2022-07-04 13:45:51 +0200

    werkend met database online

--- felix.py
+++ felix.py
@@ -1,27 +1,22 @@
 import mysql.connector
 from flask import jsonify
 
+mydb = mysql.connector.connect(
+	host="2206-bezorgapp.mysql.database.azure.com",  #port erbij indien mac
+	port=3306,
+	user="beheerder@2206-bezorgapp",
+	password="abcd1234ABCD!@#$",
+	database="bezorgappyc2206"
+)
 def methodevanfelix():
 	print("naar de database")
-	mydb = mysql.connector.connect(
-		host="localhost",  #port erbij indien mac
-		user="root",
-		password="",
-		database="patat"
-	)
 	mycursor = mydb.cursor()
-	mycursor.execute("SELECT * FROM quote")
+	mycursor.execute("SELECT * FROM maaltijd")
 	recordset = mycursor.fetchall()
 	print(recordset)
 	return jsonify(list(recordset))
 
 def aanmakenquote(denaam):
-	mydb = mysql.connector.connect(
-		host="localhost",  #port erbij indien mac
-		user="root",
-		password="",
-		database="patat"
-	)
 	mycursor = mydb.cursor()
 	sql = "INSERT INTO quote (tekst, auteur) VALUES (%s, %s)"
 	val = (denaam, "harry")
