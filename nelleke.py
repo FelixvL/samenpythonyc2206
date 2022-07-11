d8f0c6dc8cc92d910c4689c08ae56c884609a9fb
99nel <nvdoorn@gmail.com>
2022-07-11 14:09:53 +0200

    functies toegevoegd

--- nelleke.py
+++ nelleke.py
@@ -5,6 +5,7 @@ import bs4
 import requests
 import json
 import re
+import random
 
 # import data
 try:
@@ -17,7 +18,7 @@ mydb = mysql.connector.connect(
 	port=3306,
 	user="beheerder@2206-bezorgapp",
 	password="abcd1234ABCD!@#$",
-	database="bezorgappyc2206"
+	database="bezorgappbigdata"
 )
 
 def toonDataDeel():     
@@ -30,7 +31,37 @@ def toonDataSpecifiek(num):
     print(toon)
     return toon.to_json()
 
-def toonQuotes():    
+def toonQuotesAllemaal():    
+    try: # errorhandling
+        rg = requests.get("https://medium.com/swlh/21-of-the-worlds-most-powerful-quotes-updated-for-today-and-tomorrow-6b7634358c2") # GET document from medium.com        
+    except Exception as err: 
+        print("Something went wrong:", err)
+        rg = None
+        
+    html_doc = rg.text # get HMTL content from response object
+    soup = bs4.BeautifulSoup(html_doc, 'html.parser') # turn html_doc into BS4 object
+    quotes = soup.find_all("h2") # find all quotes  
+    
+    counter = 0
+    quotes_lijst = []
+    
+    for q in quotes[:21]:
+        res = str(q).split("“")
+        res2 = res[1][:-5]
+        res3 = res2.split("” — ")
+        
+        counter +=1
+        tekst_en_auteur = (f"{counter}: {res3[0]} - {res3[1]}")
+        quotes_lijst.append(tekst_en_auteur)
+        
+    print(quotes_lijst)
+
+# def randomQuote(nummer):
+#     nummer = random.randint(0, 22)
+#     return "Hi,  quotes_lijst.get(nummer)
+
+
+def opslaanQuotes():    
     try: # errorhandling
         rg = requests.get("https://medium.com/swlh/21-of-the-worlds-most-powerful-quotes-updated-for-today-and-tomorrow-6b7634358c2") # GET document from medium.com        
     except Exception as err: 
@@ -41,38 +72,19 @@ def toonQuotes():
     soup = bs4.BeautifulSoup(html_doc, 'html.parser') # turn html_doc into BS4 object
     quotes = soup.find_all("h2") # find all quotes  
         
-    for q in quotes[:21]:
-        res = str(q).split("“")
-        res2 = res[1][:-6]
+    for quote in quotes[:21]:
+        res = str(quote).split("“")
+        res2 = res[1][:-5]
         res3 = res2.split("” — ")
-        print("quote: ",res3[0], "\nauteur: ", res3[1],"\n\n")
-
-    return "scrapen van website en tonen van quotes in console is gelukt"
-
-# def opslaanQuotes():    
-#     try: # errorhandling
-#         rg = requests.get("https://medium.com/swlh/21-of-the-worlds-most-powerful-quotes-updated-for-today-and-tomorrow-6b7634358c2") # GET document from medium.com        
-#     except Exception as err: 
-#         print("Something went wrong:", err)
-#         rg = None
-        
-#     html_doc = rg.text # get HMTL content from response object
-#     soup = bs4.BeautifulSoup(html_doc, 'html.parser') # turn html_doc into BS4 object
-#     quotes = soup.find_all("h2") # find all quotes  
-        
-#     for quote in quotes[:21]:
-#         res = str(quote).split("“")
-#         res2 = res[1][:-6]
-#         res3 = res2.split("” — ")
     
-#         q = res3[0]
-#         a = res3[1]
+        q = res3[0]
+        a = res3[1]
     
-#         mycursor = mydb.cursor()
-#         sql = "INSERT INTO quote (quote, auteur) VALUES (%s, %s)"
-#         val = (q, a)
-#         mycursor.execute(sql, val)
-#         mydb.commit()
-#         print(mycursor.rowcount, "record inserted.")
+        mycursor = mydb.cursor()
+        sql = "INSERT INTO quote (tekst, auteur) VALUES (%s, %s)"
+        val = (q, a)
+        mycursor.execute(sql, val)
+        mydb.commit()
+        print(mycursor.rowcount, "record inserted.")
     
-#     return "opslaan van quotes is gelukt"
+    return "opslaan van quotes is gelukt"
